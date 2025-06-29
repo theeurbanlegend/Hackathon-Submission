314d4e748f17c05ff2d425004cdb87f1cd934c5b
theeurbanlegend <mbuto152@gmail.com>
2025-06-29 21:15:29 +0300

    feat: refactor contract service to initialize script address and cbor from plutus.json

--- server/src/modules/cardano/contracts/contract.service.ts
+++ server/src/modules/cardano/contracts/contract.service.ts
@@ -1,16 +1,57 @@
 import { Injectable } from '@nestjs/common';
-import { scriptCbor, scriptAddr } from './generateScriptAddress';
-import { BillDocument } from 'src/entities/bill.entity';
 import { byteString, conStr, deserializeAddress } from '@meshsdk/core';
+import * as fs from 'fs';
+import * as path from 'path';
+import { applyParamsToScript, serializePlutusScript } from '@meshsdk/core';
+import { CardanoNetwork } from 'src/common/constants';
+import { BillDocument } from 'src/entities/bill.entity';
 
 @Injectable()
 export class ContractService {
+  private scriptAddress: string;
+  private scriptCbor: string;
+  constructor() {
+    this.init();
+  }
+
   getScriptAddress(): string {
-    return scriptAddr;
+    return this.scriptAddress;
   }
 
   getContractCbor(): string {
-    return scriptCbor;
+    return this.scriptCbor;
+  }
+
+  private init() {
+    if (!this.scriptAddress || !this.scriptCbor) {
+      try {
+        console.log('Initializing script address and cbor...');
+        const blueprint = JSON.parse(
+          fs.readFileSync(
+            path.join(
+              process.cwd(),
+              'src/modules/cardano/contracts',
+              'plutus.json',
+            ),
+            'utf-8',
+          ),
+        );
+
+        this.scriptCbor = applyParamsToScript(
+          blueprint.validators[0].compiledCode,
+          [],
+        );
+
+        this.scriptAddress = serializePlutusScript(
+          { code: this.scriptCbor, version: 'V3' },
+          undefined,
+          CardanoNetwork.Testnet,
+        ).address;
+      } catch (error) {
+        console.error('Error initializing contract service:', error);
+        throw new Error('Failed to initialize contract service');
+      }
+    }
   }
 
   getPaymentVerificationKeyFromBech32(bech32Address: string): string {
